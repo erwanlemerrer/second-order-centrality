03424cbc2bccaeefabbaffb3f7d69356116a9f21
Erwan Le Merrer <erwan.lemerrer@technicolor.com>
2018-08-10 15:19:19 +0200

    clear BSD license

--- second_order_centrality.py
+++ second_order_centrality.py
@@ -1,22 +1,32 @@
 # -*- coding: utf-8 -*-
-# /********** COPYRIGHT AND CONFIDENTIALITY INFORMATION NOTICE *************
-# ** Copyright (c) 2017 Thomson Licensing – a Technicolor Group Company  **
-# ** All Rights Reserved                                                  **
-# ** Technicolor hereby informs you that the following portions of this   **
-# ** open source module and/or Work are distributed and not sub-licensed  **
-# ** by Technicolor, under the terms of the applicable open source        **
-# ** software license terms (available from the Readme files).            **     
-# ** This Work may or may not include modifications or additions.         **
-# ** Technicolor modifications and/or additions shall in no event be      **
-# ** deemed construed or otherwise considered as being a "Contribution"   **
-# ** to such third party Apache 2.0 open source software code.            **
-# ** Distribution and copying of all such modifications are reserved      **
-# ** to Technicolor and/or its affiliates, and are not permitted without  **
-# ** express written authorization from Technicolor.                      **
-# ** Technicolor is registered trademark and trade name of Technicolor,   **
-# ** and shall not be used in any manner without express written          **
-# ** authorization from Technicolor                                       **
-# **************************************************************************/
+# Copyright (c) 2015 – Thomson Licensing, SAS                                                   
+#                                                                                               
+# Redistribution and use in source and binary forms, with or without modification,              
+# are permitted (subject to the limitations in the disclaimer below) provided that              
+# the following conditions are met:                                                             
+#                                                                                               
+# * Redistributions of source code must retain the above copyright notice, this                 
+# list of conditions and the following disclaimer.                                              
+#                                                                                               
+# * Redistributions in binary form must reproduce the above copyright notice,                   
+# this list of conditions and the following disclaimer in the documentation                     
+# and/or other materials provided with the distribution.                                        
+#                                                                                               
+# * Neither the name of Thomson Licensing, or Technicolor, nor the names of its                 
+# contributors may be used to endorse or promote products derived from this                     
+# software without specific prior written permission.                                           
+#                                                                                               
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS               
+# LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS                 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,                 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE                
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE               
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL                    
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR                    
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER                    
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR              
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF                 
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
 #
 # Authors: Erwan Le Merrer (erwan.lemerrer@technicolor.com)
 ''' Second order centrality measure.'''
